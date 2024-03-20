4f8f6b617fc26933d28206fc7621846db8ae1c63
Tarick Arnold <120078573+tarickarnold@users.noreply.github.com>
2024-03-20 12:06:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import datetime
 import pandas as pd
 
+
 # Function to filter column by dates less than current date and specific email address
-def filter_excel(file_path, email_address):
-    
+def filter_excel(file_path: str, email_address: str) -> str:
+        
     # Load the workbook
-    df = pd.read_excel(file_path)
+    df: str = pd.read_excel(file_path)
     
     # Get the current date
-    current_date = datetime.datetime.today()
+    current_date: datetime = datetime.datetime.today()
     
     # Convert string date text in dataframe to date format
-    df['date'] = pd.to_datetime(df['date'], format="%m/%d/%Y")
+    df["date"]: str = pd.to_datetime(df["date"], format="%m/%d/%Y")
     
     # Filter data frame to show all dates less than or equal to today
-    filtered_df = df[df['date'] <= current_date]
+    filtered_df: datetime = df[df["date"] <= current_date]
     
     # Filter dates by email_address variable
-    filtered_emails = filtered_df.loc[(filtered_df['email'] == email_address)]
+    filtered_emails: str = filtered_df.loc[(filtered_df["email"] == email_address)]
 
-    email_body = filtered_emails.to_html()
+    # Transform dataframe into HTML table
+    email_body: str = filtered_emails.to_html()
     
-    return email_body
+    #return email_body
+    print(email_body)
+
+def main():
     
+    file_path: str = "YOUR FILE PATH HERE"
+    email_address: str = "YOUR EMAIL HERE"
+    filter_excel(file_path, email_address)    
 
 if __name__ == "__main__":
-
-    file_path = ""  # Replace with your file path
-    email_address = ""  # Replace with the email address to filter by
-
-    filter_excel(file_path, email_address)
+    main()
